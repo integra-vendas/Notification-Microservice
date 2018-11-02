b56708f77b8f5cf96c7fa08c3da77a6162ef6b50
Andre Lucas <andrelucax@gmail.com>
2018-11-01 22:20:35 -0300

    Created test for ProfileToken.
    Co-authored-by: André Lucas de Sousa Pinto <andrelucax@gmail.com>
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>

--- notification_microservice/notifications/tests.py
+++ notification_microservice/notifications/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import ProfileToken
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class ProfileTokenTest(APITestCase):
+    def test_without_params(self):
+        #Checking POST without any
+        profile = {'test': 'test'}
+        response = self.client.post('/api/save_user_token/', profile)
+        self.assertEqual(response.status_code, 400)
+
+        #Checking POST with user_id and no user_token
+        profile = {'user_id': '1'}
+        response = self.client.post('/api/save_user_token/', profile)
+        self.assertEqual(response.status_code, 400)
+
+        #Checking POST with user_token and no user_id
+        profile = {'user_token': 'ThisIsAToken'}
+        response = self.client.post('/api/save_user_token/', profile)
+        self.assertEqual(response.status_code, 400)
+
+    def test_new_profile(self):
+        #Checking CREATE new profile token
+        profile = {'user_token': 'ThisIsAToken', 'user_id': '1'}
+        response = self.client.post('/api/save_user_token/', profile)
+        self.assertEqual(response.status_code, 200)
+
+        profile = {'user_token': 'OtherToken', 'user_id': '2'}
+        response = self.client.post('/api/save_user_token/', profile)
+
+        profile1 = ProfileToken.objects.all()[0]
+        profile2 = ProfileToken.objects.all()[1]
+
+        self.assertEqual(profile1.user_token, 'ThisIsAToken')
+        self.assertEqual(profile1.user_id, 1)
+
+        self.assertEqual(profile2.user_token, 'OtherToken')
+        self.assertEqual(profile2.user_id, 2)
